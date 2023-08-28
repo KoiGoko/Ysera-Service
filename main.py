c7ab55ad8157bb1adcd748954f01151536ed8bcd
KoiGoko <768514806@qq.com>
2023-08-28 14:25:27 +0800

    重构

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from typing import Union
+from fastapi_amis_admin.admin.settings import Settings
+from fastapi_amis_admin.admin.site import AdminSite
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +15,13 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+# # 创建AdminSite实例
+# site = AdminSite(settings=Settings(database_url_async='sqlite+aiosqlite:///amisadmin.db'))
+#
+# # 挂载后台管理系统
+# site.mount_app(app)
+
+if __name__ == '__main__':
+    uvicorn.run(app)
+
