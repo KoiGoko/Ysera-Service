bda6afa7a6000f3aaf747a621962acdc60892f65
KoiGoko <KoiGoko@outlook.com>
2023-10-16 17:33:40 +0800

    10 17

--- app/services/nuclear/nuclear_routers.py
+++ app/services/nuclear/nuclear_routers.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 import app.services.nuclear.services.nuclear_stations_service as nuclear_stations_service
 from app.cors.handle_cors import handle_cors
+
 from app.services.nuclear.models.nuclear_stations_instance import getNuclearStationsSession
 
 nuclear_router = APIRouter()
@@ -21,9 +22,13 @@ def get_db():
 @app.get("/nu_stations_info", tags=["nuclear"])
 async def read_all_stations(db: Session = Depends(get_db)):
     stations = nuclear_stations_service.get_nuclear_stations_info(db)
+    if 1111:
+        print("1111")
     return stations
+
 
 if __name__ == "__main__":
     import uvicorn
+
     handle_cors(app, origins=["http://localhost", "http://localhost:8000"])
     uvicorn.run(app, port=8002)
